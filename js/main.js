6bd332edd441b709f9264de7ea85bc7ec0609757
ColeyG <cjgeerts@gmail.com>
2017-09-20 02:27:35 -0400

    typed.js integration for motion text

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 (function(){
-
+    var options = {
+        strings: [
+        "I am a <span class='typeChange'>writer,</span>^1000",
+        "I am a <span class='typeChange'>designer,</span>^1000",
+        "I am a <span class='typeChange'>developer,</span>^1000"
+        ],
+        startDelay: 2000,
+        typeSpeed: 40,
+        backSpeed: 40,
+        loop:true,
+        smartBackspace:true
+        /*Add comma points and extra typed.js attributes here.
+        Code is found at: https://github.com/mattboldt/typed.js/*/
+      }
+      
+      var typed = new Typed(".moving", options);
+      
 })();
