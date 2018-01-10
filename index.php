0a18eb56a388572281f6695bb4e35071a4f9639d
Geerts <c_geerts@tss-hr420-dm18.local>
2018-01-09 19:20:13 -0500

    fullAJAX

--- index.php
+++ index.php
@@ -22,45 +22,6 @@ $check="";
             }
     }
 }
-
-
-$user = 'root';
-$password = 'root';
-$db = 'db_portfolio';
-$host = 'localhost';
-$port = 3306;
-
-$link = mysqli_init();
-$success = mysqli_real_connect(
-   $link, 
-   $host, 
-   $user, 
-   $password, 
-   $db,
-   $port
-);
-
-
-  if(mysqli_connect_errno()){
-       printf("Connection failed: %s\n", mysqli_connect_error());
-    exit();
-  }
-  $query="SELECT project_name_short, project_name_long, project_image, project_desc, project_link, project_date FROM `tbl_projects` ORDER BY project_date DESC";
-  $get=mysqli_query($link,$query);
-
-  $grpResult="";
-
-  while ($result = mysqli_fetch_assoc($get)) {
-    //echo $movResult['movies_id'];
-    $jsonResult = json_encode($result);
-    $grpResult .=$jsonResult.",";
-    
-}   
-    $grpResult=substr($grpResult,0,-1);
-    $grpResult="[".$grpResult."]";
-
-  mysqli_close($link);
-
 ?>
 
 <!doctype html>
--- js/serverfiller.js
+++ js/serverfiller.js
@@ -28,35 +28,21 @@
     }
 
     function processResult(data){
-        for(i=0;i<projects.length;i++){
-        var ns=projects[i].project_name_short;
-        var nl=projects[i].project_name_short;
-        var img=projects[i].project_image;
-        var desc=projects[i].project_desc;
-        var link=projects[i].project_link;
-        var date=projects[i].project_date;
+        for(i=0;i<data.length;i++){
+        var ns=data[i].project_name_short;
+        var nl=data[i].project_name_short;
+        var img=data[i].project_image;
+        var desc=data[i].project_desc;
+        var link=data[i].project_link;
+        var date=data[i].project_date;
         cards=cards+"<a href='"+link+"'><div class='card'><div class='cardImg'><img src='images/projectHeaders/"+img+"'></div><h2 class='cardTitle'>"+ns+"</h2><h2 class='cardSubTitle'>"+desc+"</h2><h2 class='cardDate'>"+date+"</h2></div></a>";
         cardArea[0].innerHTML=cards;
         }
     }
 
-    var projects=JSON.parse(document.getElementById('jsonObject').innerHTML);
     var cardArea=document.querySelectorAll('.cardBanner');
 
-    console.log(projects);
-
     var cards='';
-
-    /*for(i=0;i<projects.length;i++){
-        var ns=projects[i].project_name_short;
-        var nl=projects[i].project_name_short;
-        var img=projects[i].project_image;
-        var desc=projects[i].project_desc;
-        var link=projects[i].project_link;
-        var date=projects[i].project_date;
-        cards=cards+"<a href='"+link+"'><div class='card'><div class='cardImg'><img src='images/projectHeaders/"+img+"'></div><h2 class='cardTitle'>"+ns+"</h2><h2 class='cardSubTitle'>"+desc+"</h2><h2 class='cardDate'>"+date+"</h2></div></a>";
-        cardArea[0].innerHTML=cards;
-    }*/
-
+    
     loading();
 })();
